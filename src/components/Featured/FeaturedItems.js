8ce446afbd616bea57e67e31c2cb1e8022c4ed1b
gpasxalis <gpasxalis94@gmail.com>
2024-04-04 15:24:35 +0300

    fixes

--- src/components/Featured/FeaturedItems.js
+++ src/components/Featured/FeaturedItems.js
@@ -3,7 +3,7 @@ import FeaturedItem from "./FeaturedItem";
 
 export default function FeaturedItems(props) {
 	return (
-		<div className="featured">
+		<>
 			<h3 className="featured--title">{ props.title }</h3>
 			<FeaturedItem 
 				featured_title="Το μενού μας" 
@@ -18,6 +18,6 @@ export default function FeaturedItems(props) {
 				featured_url="https://gr.linkedin.com/in/paschalis-grammenos-a4b430114"
 				featured_button="Δείτε το εδώ"
 			/>
-    	</div>
+    	</>
   	);
 }
