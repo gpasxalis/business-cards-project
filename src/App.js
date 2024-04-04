789fad01bba212eb316a2ccaf396675021fb5d52
gpasxalis <gpasxalis94@gmail.com>
2024-04-04 09:58:43 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import MainSection from "./components/MainSection";
-import About from "./components/About";
-import Interests from "./components/Interests";
 import Footer from "./components/Footer";
 import Featured from "./components/Featured";
 
