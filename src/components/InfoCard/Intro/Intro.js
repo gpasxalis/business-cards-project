c0480ef1dc6765ad713096dd35d66ac6cc209af1
gpasxalis <gpasxalis94@gmail.com>
2024-04-05 00:52:41 +0300

    fixes

--- src/components/InfoCard/Intro/Intro.js
+++ src/components/InfoCard/Intro/Intro.js
@@ -1,19 +1,10 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< HEAD
-import {  faIdBadge } from "@fortawesome/free-solid-svg-icons";
-import IntroInfoButtons from "./IntroInfoButtons";
-import IntroInfoButtonsSocial from "./IntroInfoButtonsSocial";
-import IntroInfo from "./IntroInfoText";
-import ContactVCF from "../../Misc/vcard.vcf"
-
-=======
 import { faIdBadge } from "@fortawesome/free-solid-svg-icons";
 import IntroInfoButtons from "./IntroInfoButtons";
 import IntroInfoButtonsSocial from "./IntroInfoButtonsSocial";
 import IntroInfo from "./IntroInfoText";
 import ContactVCF from "../../Misc/vcard.vcf";
->>>>>>> f44f8d1 (Updates)
 
 export default function Intro() {
   return (
@@ -29,24 +20,12 @@ export default function Intro() {
           download
         >
           <div className="icon iconColor">
-<<<<<<< HEAD
-            <FontAwesomeIcon icon={faIdBadge} className="info--icon" /> 
-          </div>
-          <p>
-            Save Contact
-          </p>
-          
-        </a>
-      </div>
-      
-=======
             <FontAwesomeIcon icon={faIdBadge} className="info--icon" />
           </div>
           <p>Save Contact</p>
         </a>
       </div>
 
->>>>>>> f44f8d1 (Updates)
       <IntroInfoButtons />
 
       <IntroInfoButtonsSocial />
