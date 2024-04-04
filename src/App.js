76e32e43d7a38bdd1e7d6f08d8b2e974f4c62b99
gpasxalis <gpasxalis94@gmail.com>
2024-04-05 01:00:11 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
 import BCard from "./components/BCard";
+import Modal from "./components/Misc/Modal";
 
 export default function App() {
   return (
-    <div className="card">
-      <BCard />
-      <Footer />
-    </div>
+    <>
+      <Modal />
+      <div className="card">
+        <BCard />
+        <Footer />
+      </div>
+    </>
   );
 }
