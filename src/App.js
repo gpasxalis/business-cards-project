17f7ab35f6d1a7fadbbe2dd12e8f76d7559f2362
gpasxalis <gpasxalis94@gmail.com>
2024-04-05 08:08:00 +0300

    Hot Fixes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import BCard from "./components/BCard";
 import Modal from "./components/Misc/Modal";
+import ShareActions from "./components/ShareActions/ShareActions";
 
 export default function App() {
   return (
     <>
       <Modal />
+      <ShareActions />
       <div className="card">
         <BCard />
         <Footer />
